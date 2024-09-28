19f4c121ad00402f34e737918ed149bd904bfcc4
Veera <168694187+Veera1011@users.noreply.github.com>
2024-09-28 16:13:59 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Projects from './components/Projects';
-import Resume from './components/Resume';
+import About from './About';
+import Contact from './Contact';
+import Footer from './Footer';
+import Header from './Header';
+import Hero from './Hero';
+import Projects from './Projects';
+import Resume from './Resume';
 
 function App() {
   return (
